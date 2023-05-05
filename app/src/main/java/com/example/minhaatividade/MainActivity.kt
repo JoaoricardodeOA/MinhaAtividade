65f0e2896327e7ceff195e78d8a451add9216f25
Administrador <joaoricardoagostinho285@gmail.com>
2023-05-04 21:00:06 -0300

    added firebase

--- app/src/main/java/com/example/minhaatividade/MainActivity.kt
+++ app/src/main/java/com/example/minhaatividade/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.minhaatividade.databinding.ActivityMainBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding : ActivityMainBinding
@@ -20,6 +22,10 @@ class MainActivity : AppCompatActivity() {
 
 
         bt.setOnClickListener{
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
             if (ed.text.toString().equals("joao") && senha.text.toString().equals("123")){
                 val texto = ed.text.toString()
                 val intent = Intent(this, LazerActivity::class.java)
